b6a86842315dbdeb9572f887d05e23050852cc11
Deepali18Kalra <114229530+Deepali18Kalra@users.noreply.github.com>
2024-09-04 16:57:32 +0000

    star, number and alphabet patterns

---
+++ Pattern.java
@@ -0,0 +1,63 @@
+class Pattern{
+    public static void main(String[] args){
+        for(int i=0;i<=4;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+
+        for(int i=0;i<=4;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print((char)(65+j));
+            }
+            System.out.println("");
+        }
+
+        for(int i=0;i<=4;i++){
+            for(int j=4;j>=0;j--){
+                if(j>i){
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println("");
+        }
+
+        for(int i=1;i<=5;i++){
+            int k=1;
+            for(int j=5;j>=1;j--){
+                if(j>i){
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print(k++);
+                }
+            }
+            System.out.println("");
+        }
+
+        for(int i=1;i<=5;i++){
+            int k=0;
+            for(int j=5;j>=1;j--){
+                if(j>i){
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print((char)(65+(k++)));
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+}
