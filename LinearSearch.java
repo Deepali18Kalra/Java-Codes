4f92237c063aa1d8f10d8abca143c5b7e58684da
Deepali18Kalra <114229530+Deepali18Kalra@users.noreply.github.com>
2024-09-05 18:36:05 +0000

    linear search on array

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,5 +1,32 @@
+import java.util.*;
 class LinearSearch{
     public static void main(String[] args){
-        
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the size of the array - ");
+        int length = scn.nextInt();
+        int[] arr = new int[length];
+
+        for(int i=0;i<length;i++){
+            System.out.println("Enter element "+ (i+1));
+            arr[i]=scn.nextInt();
+        }
+
+        System.out.println("Enter the key - ");
+        int key = scn.nextInt();
+
+        int keyIndex = -1;
+        for(int i=0;i<length;i++){
+            if(arr[i]==key){
+                keyIndex=i;
+                break;
+            }
+        }
+
+        if(keyIndex==-1){
+            System.out.println("Key not found in the array.");
+        }
+        else{
+            System.out.println("Key found at position "+(keyIndex+1));
+        }
     }
 }
