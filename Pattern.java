92629e59a2d8a2aec2af9271d07125198cb7b852
Deepali18Kalra <114229530+Deepali18Kalra@users.noreply.github.com>
2024-09-05 16:29:57 +0000

    star, alphabet, number patterns in java

--- Pattern.java
+++ Pattern.java
@@ -58,6 +58,27 @@ class Pattern{
             }
             System.out.println("");
         }
+
+        for(int i=5;i>=1;i--){
+            for(int j=1;j<=5;j++){
+                if(j<i){
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print(j-i+1);
+                }
+            }
+            System.out.println("");
+        }
+
+        int k=0;
+        for(int i=0;i<=3;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(++k);
+            }
+            System.out.println("");
+        }
+
     }
 
 }
